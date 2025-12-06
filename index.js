9452e414c8495849877128602261340c4693bf6a
o5sy <oosydev@gmail.com>
2025-12-06 16:56:48 +0900

    Chore: 배포 테스트 스크립트 추가

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Deployment test: server started.");
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write("Hello from Render!");
+  res.end();
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
